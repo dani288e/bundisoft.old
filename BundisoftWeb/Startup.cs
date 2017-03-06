f7b392deb5ab46880d55f02adfbed0345ee608f6
dani288e <daniel_holm@live.dk>
2017-03-06 22:57:52 +0100

    initial commit

---
+++ BundisoftWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BundisoftWeb.Startup))]
+namespace BundisoftWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
